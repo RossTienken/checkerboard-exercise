bb3a3b1802a5f7f17a26dc55622bab0d826e11d6
Ross <ross.tienken99@gmail.com>
2017-10-12 17:14:57 -0600

    flashing

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+tileArr = [];
 for(var i=0;i<81;i++){
 var tile = document.createElement('div');
 var body = document.getElementsByTagName('body')[0];
+var random = '#'+Math.random().toString(16).slice(-6);
 document.body.prepend(tile);
 tile.style.width = '11.1%';
 tile.style.paddingBottom = '11.1%';
 tile.display = 'float';
 tile.style.float = 'left';
-if(i%2 === 0){
-tile.style.backgroundColor = 'blue';
-}
-
+tileArr.push(tile);
+tile.style.backgroundColor = random
 };
-
-var doc = document.getElementsByTagName('body')[0];
-doc.style.background = "linear-gradient(to right,green,teal,blue,indigo,violet, magenta)";
+function tileReplace(){
+  var random = '#'+Math.random().toString(16).slice(-6)
+  var randomIndex = Math.ceil(Math.random()*100)
+  var newTileColor = tileArr[randomIndex].style.backgroundColor = random
+}
+var intervalID = window.setInterval(tileReplace, 10);

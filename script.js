8ff1600f487106e8104f4044f32355bb1531db6e
Ross <ross.tienken99@gmail.com>
2017-10-11 18:28:18 -0600

    random colors

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 for(var i=0;i<81;i++){
 var tile = document.createElement('div');
 var body = document.getElementsByTagName('body')[0];
+var random = '#'+Math.random().toString(16).slice(-6);
 document.body.prepend(tile);
 tile.style.width = '11.1%';
 tile.style.paddingBottom = '11.1%';
 tile.display = 'float';
 tile.style.float = 'left';
 if(i%2 === 0){
-tile.style.backgroundColor = 'black';
+tile.style.backgroundColor = random;
 }
-
+if(i%2 !== 0){
+  tile.style.backgroundColor = random;
+}
+console.log(random)
 };
-
-var doc = document.getElementsByTagName('body')[0];
-doc.style.background = 'red';
